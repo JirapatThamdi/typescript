557008f4132f92ea4142cee003a67585d602faa3
Jirapat Thamdi <plam2544@hotmail.com>
2025-01-15 17:46:25 +0700

    add return type of function

--- test.ts
+++ test.ts
@@ -11,22 +11,24 @@ type Order = {
     status: 'ordered' | 'completed'
 }
 
-const menu: Pizza[] = [
-    { id: 1, name: 'Pepperoni', price: 15 },
-    { id: 2, name: 'Margherita', price: 10 },
-]
-
 // Variable that use to edit need to be declared as let
 // const in other name is a constant variable or readonly variable
 let cashInRegister: number = 100
 let nextOrderId: number = 1
+let nextPizzaId: number = 1
 const orderQueue: Order[] = []
 
-const addNewPizza = (pizzaObj: Pizza) => {
+const menu: Pizza[] = [
+    { id: nextPizzaId++, name: 'Pepperoni', price: 15 },
+    { id: nextPizzaId++, name: 'Margherita', price: 10 },
+]
+
+// Void is used when the function does not return anything
+const addNewPizza = (pizzaObj: Pizza): void => {
     menu.push(pizzaObj)
 }
 
-const placeOrder = (pizzaName: string) => {
+const placeOrder = (pizzaName: string): Order | undefined => {
     // Triple equals is used to check if the value and type are the same
     // If double equals is used, it will only check the value are the same
     const selectedPizza = menu.find((pizza) => pizza.name === pizzaName)
@@ -45,7 +47,7 @@ const placeOrder = (pizzaName: string) => {
     return newOrder
 }
 
-const completeOrder = (orderId: number): Order => {
+const completeOrder = (orderId: number): Order | undefined => {
     const order = orderQueue.find((order) => order.id === orderId)
 
     // Always check if variable exits after using some method
@@ -59,7 +61,9 @@ const completeOrder = (orderId: number): Order => {
     return order
 }
 
-export const getPizzaDetail = (identifier: string | number): Pizza => {
+// Without declare return type, typescript will automatically detect the return type
+// In this case it will be Pizza | undefined
+export const getPizzaDetail = (identifier: string | number): Pizza | undefined => {
     if (typeof identifier === 'number') {
         return menu.find((pizza) => pizza.id === identifier)
     } else if (typeof identifier === 'string') {
@@ -69,9 +73,9 @@ export const getPizzaDetail = (identifier: string | number): Pizza => {
     }
 }
 
-addNewPizza({ id: 3, name: 'Hawaiian', price: 20 })
-addNewPizza({ id: 4, name: 'Veggie', price: 18 })
-addNewPizza({ id: 5, name: 'Meat Feast', price: 22 })
+addNewPizza({ id: nextPizzaId++, name: 'Hawaiian', price: 20 })
+addNewPizza({ id: nextPizzaId++, name: 'Veggie', price: 18 })
+addNewPizza({ id: nextPizzaId++, name: 'Meat Feast', price: 22 })
 
 placeOrder('Pepperoni')
 completeOrder(1)
